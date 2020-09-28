b189a19288688c1579e703288febcec779eaaeb7
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2020-09-28 21:30:06 +0200

    Lint with black

--- docs/conf.py
+++ docs/conf.py
@@ -13,17 +13,17 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'open_plan'
-copyright = '2020, Reiner Lemoine Institut'
-author = 'Reiner Lemoine Institut'
+project = "open_plan"
+copyright = "2020, Reiner Lemoine Institut"
+author = "Reiner Lemoine Institut"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -42,12 +42,12 @@ extensions = [
     "numpydoc",
 ]
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -64,4 +64,4 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
