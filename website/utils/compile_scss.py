394847acad0c1caeb7fdfc56d56bc0269f2fa9b1
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2020-11-06 14:54:24 +0100

    Script to convert scss files to css

---
+++ website/utils/compile_scss.py
@@ -0,0 +1,34 @@
+from scss import Compiler
+import os
+
+
+def convert_scss_to_css(file_name):
+    """
+
+    Parameters
+    ----------
+    file_name: path or list of path
+
+    Returns
+    -------
+    convert file(s) in path or in all paths from "scss" to
+
+    """
+    if isinstance(file_name, list):
+        for file in file_name:
+            convert_scss_to_css(file)
+    else:
+
+        if file_name.endswith("scss") is False:
+            raise TypeError(
+                f"The file {file_name} is not of type scss, please verify its extension"
+            )
+
+        with open(file_name) as scss_file:
+            scss_str = scss_file.read().replace("\n", " ")
+
+        css_str = Compiler(output_style="compressed").compile_string(scss_str)
+        file_name = file_name.replace(".scss", ".css")
+
+        with open(file_name, "w") as css_file:
+            css_file.write(css_str)
