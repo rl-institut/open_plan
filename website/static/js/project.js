6eab80248d206a31976280d903a87d3834390e36
bmlancien <bryan.lancien@rl-institut.de>
2020-11-17 16:12:22 +0100

    add arrow to EC section

--- website/static/js/project.js
+++ website/static/js/project.js
@@ -2,10 +2,12 @@ function showHide(id, btn){
     let div = document.getElementById(id);
     let showBtn = document.getElementById(btn);
     if (div.className === "hidden") {
-        div.className = "hidden  active";
-        showBtn.innerHTML = "hide";
+        div.className = "hidden active";
+        showBtn.className = "hidden active";
+        showBtn.innerHTML = "Hide";
     } else {
         div.className = "hidden";
-        showBtn.innerHTML = "show more";
+        showBtn.className = "hidden";
+        showBtn.innerHTML = "Show more";
     }
 }
